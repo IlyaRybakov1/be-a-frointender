db16a4ca68204e806204b822889fcab8b75fa148
IlyaRybakov1 <ilyarybakov6@gmail.com>
2023-11-25 01:24:28 +0300

    Hometask done

--- server.js
+++ server.js
@@ -1,34 +1,105 @@
-// const http = require("http");
+const express = require("express");
+   
+const app = express();
 
-// const host = '127.0.0.1';
-// const port = 8000;
 
-// const serverListener = (req, res) => {
-//   console.log('got request', req);
-//   res.writeHead(200);
-//   res.end('Your response');
-// }
+let data = [
+  { id: 10, firstName:"Marty", lastName: "McFly", group: 101, rate: 9.5 },
+  { id: 11, firstName:"Squidward", lastName: "Tentakles", group: 102, rate: 6.1 },
+  { id: 12, firstName:"Donald", lastName: "Duck", group: 102, rate: 7.2 },
+  { id: 13, firstName:"Sarah", lastName: "Connor", group: 101, rate: 8.3 },
+  { id: 14, firstName:"Yugin", lastName: "Krabbs", group: 102, rate: 6.8 },
+ ];
+ 
+//парсер для данных
+const urlencodedParser = express.urlencoded({extended: false});
 
-// const server = http.createServer(serverListener);
 
-// server.listen(port, host, () => {
-//   console.log('server started at port ' + port)
-// });
+app.get("/students", function (req, res) {
+  let json = { ...data };
+    res.send(json);
+});
 
-const express = require('express')
-const app = express()
-const port = 8000
-const router = require('./router')
+app.get("/students/:id", function (req, res) {
+  let sendData = "Cannot find student with id " + req.params.id;
+    data.forEach((element) => {
+      if (req.params.id == element.id) {
+        sendData = element;
+      }
+    });
+    res.send(sendData);
+});
 
-const JSONexample = { a:1, bb:222, cc: [1,1,2,2,3], d: { a: 'AAAA' }};
+app.post("/students/:id", urlencodedParser, function (req, res) {
+  if (req.body.firstName.length == 0 || req.body.lastName.length == 0) {
+    res.sendStatus(400);
+  } else {
+    let isError = false;
+    data.forEach((element) => {
+      if (req.params.id == element.id) {
+        res.sendStatus(400);
+        res.send("Input data error!");
+        isError = true;
+      }
+    });
+    if (!isError) {
+      let obj = {
+        id: Number(req.params.id),
+        firstName: req.body.firstName,
+        lastName: req.body. lastName,
+        group: Number(req.body.group),
+        rate: Number(req.body.rate)
+      }
+      data.push(obj);
+      console.log(data);
+      res.send("Data added!");
+    }
+  }
+});
 
-app.use(express.static('public'))
-app.use(router)
+//Студенту нельзя изменить id
+app.put("/students/:id", urlencodedParser, function (req, res) {
+  if ((req.body.firstName != undefined && req.body.firstName.length == 0) || (req.body.lastName != undefined && req.body.lastName.length == 0)) {
+    res.sendStatus(400);
+  } else {
+    let isError = true;
+    for (let i = 0 ; i < data.length; i++) {
+      if (req.params.id == data[i].id) {
+        isError = false;
+        if (req.body.firstName != undefined) {
+          data[i].firstName = req.body.firstName;
+        }
+        if (req.body.lastName != undefined) {
+          data[i].lastName = req.body.lastName;
+        }
+        if (req.body.group != undefined) {
+          data[i].group = Number(req.body.group);
+        }
+        if (req.body.rate != undefined) {
+          data[i].rate = Number(req.body.rate);
+        }
+        res.send("Data changed!");
+        break
+      }
+    }
+    if (isError) {
+      res.sendStatus(400);
+    }
+  }
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.delete("/students/:id", urlencodedParser, function (req, res) {
+  let isError = true;
+    for (let i = 0 ; i < data.length; i++) {
+      if (req.params.id == data[i].id) {
+        isError = false;
+        data.splice(i,1);
+        res.send("Deleted!");
+        break;
+      }
+    }
+    if (isError) {
+      res.sendStatus(400);
+    }
+});
+app.listen(8000);
